b030b162ba3a2f17822005e88ab205b7a16995bf
zul47 <batzul647@gmail.com>
2025-12-04 17:07:19 +0100

    Add files via upload

---
+++ aa_muu_python.py
@@ -0,0 +1,75 @@
+print('ZULAA-GIIN AA MUU PYTHON HICHEELD TAVTAI MORIL!')
+
+print('Informatik deeree bagshiin bichseniig huulaal bdg bolood ch bgm shig bolku ch bgm shig, code bicheed bnu code nmg bicheed bnu oilgohgui bolood irsiimaa.')
+
+print('Za tegeed yr ni yu suraad bgagaa oilgoh geed buur ehnees ni bugdiin google-dlee gj.')
+
+print('Tegeed medsen jijken yma ch hamaagui temdgelj avch bgd suuldee neg iim ym bolchloo hh')
+#tashramd duridahad devter deeree uurteiguu hereldehiig zaaj surgsan Anujindaa talarhal ilerhiiley <3
+
+print("Lass uns gehen")
+# # gj comment bichij bolno codend bol nuluuluhgui
+# ghee gants murund l uilchleh bolohoor 
+# mur burt # tavih heregtei gsn ug
+'''Herev ingeed ashiglaval
+Heden ch mur bsn
+Hamaagui
+Bugd comment bolno.'''
+
+
+print('za print bol oilgomjtoi ug hevlene')
+# gol ni () bolon "", '' aa martaj bolohgui
+
+# neg murund bichmeer baival , end=“” gej nemne. Ghde dund ni space bichvel 2 uguulber hoorondoo zaitai bolno gesen ug , end=“ “ geed
+print("du bist mein Schatz", end=". ")
+print("Das Leben ist sinnlos", end=" ")
+print("doch mit dir ist alles schön") 
+
+print('ug bichihdee 2 dusal zaaval bichij bgashde')
+print('aan harin too bichih bol hereggui')
+print(4); print(3 + 3); print(2 * 8)
+print('aan bas', 12345, 'geed too used holj bolno', 'gol ni taslal')
+# ghee zuger print(“bi 19 tei”) gej bichsen ch bolno l doo. zugeer bolomjit huvilbaruudiig huvaaltsjin.
+
+
+print('Variable')
+
+print('variable aa yaj bichih ve?')
+MyVar = 1
+my_var = 2
+_myVAR = 3
+# geh met husseneeree bichij bolno oo zla, anu gsn ch bolno
+print('ghde ganz bolohgui ym bn SCHADE')
+print('my-var gej dunduur zuraas ashiglaj bolkuee hu!')
+print('\n HUN NEG AMIDRALDAA DORIVTOIHON VARIABLE BICHCHEHEY GHEER DANDAA BOLKU BUTKU GEJ HUTSAJ BN')
+#dunda neg murnii zaitai bolgomoor bvl '\n' gej bichij boldishuu
+
+x = 5
+y = 'bol too' #turuun helsenchlen ug bvl ""/''-iigee martaj bolkushde bro
+print(x, y) # shuud “5 bol too” gj hevlegdnee gsn ug
+
+A = 'hello'
+a = 'hi'
+# “” bolon ‘’ bol yg adilhan aliig ni ch ashiglaj bolno
+# ghde a bolon A uur, computer 2 uur variable bn gj oilgono
+''' print(A) 
+Gevel hello garch irne
+print(a) gevel hi garch irne'''
+
+# variable-iinhaa turliig medmeer baival
+print(type(x)) # buhel too buyu 5 bolohoor “<class 'int'>” gj garch irne
+print(type(y)) #useg bolohoor "<class 'str'>" gj garch irne
+"""Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType"""
+# gsn bhn zavaarsan ym bgada yr ni
+
+
+print('daraagiin hicheeleer edgeer Datentypen bolon tednii ylgaa, ashiglalt, Vor- und Nachteile (z.B list, tuple yugaaraa uur ve?) zergiig bi uuruu chatgpt, googleeree surahiin bol bichih bha bayrlalaa, bayrtai')
+#taalagdval ndd zairmag avch uguurei angiin nuhduu
+
